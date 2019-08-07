a6cbe87a67fbd31d1c98162a5d0f236b82d1b828
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-08-07 10:05:29 -0300

    api Category finished

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,4 +6,16 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+    //cuidado na ordem das rotas, tem que vir antes /categories/:id
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+        
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
